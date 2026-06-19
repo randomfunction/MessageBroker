e8cf9bec79b4980de7cfdd179d45997fb8b396b6
randomfunction <tanishqparihar3@gmail.com>
2026-06-19 13:15:46 +0530

    cleaned code

--- MessageBroker.cpp
+++ MessageBroker.cpp
@@ -2,24 +2,26 @@
 #include <iostream>
 #include <cstring>
 
+using namespace std;
+
 void MessageBroker::subscribe(int64_t topic_id, int64_t consumer_id) {
-    std::shared_ptr<TopicData> topic_data;
+    shared_ptr<TopicData> topic_data;
     {
-        std::lock_guard<std::mutex> lock(map_mtx);
+        lock_guard<mutex> lock(map_mtx);
         if (topics.find(topic_id) == topics.end()) {
-            topics[topic_id] = std::make_shared<TopicData>();
+            topics[topic_id] = make_shared<TopicData>();
         }
         topic_data = topics[topic_id];
     }
 
-    std::lock_guard<std::mutex> lock(topic_data->topic_mtx);
+    lock_guard<mutex> lock(topic_data->topic_mtx);
     topic_data->subscribers.try_emplace(consumer_id);
 }
 
 void MessageBroker::publish(int64_t type, int64_t topic_id, const std::string& data) {
-    std::shared_ptr<TopicData> topic_data;
+    shared_ptr<TopicData> topic_data;
     {
-        std::lock_guard<std::mutex> lock(map_mtx);
+        lock_guard<mutex> lock(map_mtx);
         auto it = topics.find(topic_id);
         if (it == topics.end()) {
             return; 
@@ -33,18 +35,18 @@ void MessageBroker::publish(int64_t type, int64_t topic_id, const std::string& d
     strncpy(msg.payload, data.c_str(), sizeof(msg.payload) - 1);
     msg.payload[sizeof(msg.payload) - 1] = '\0'; 
 
-    std::lock_guard<std::mutex> lock(topic_data->topic_mtx);
+    lock_guard<mutex> lock(topic_data->topic_mtx);
     for (auto& pair : topic_data->subscribers) {
         if (!pair.second.push(msg)) {
-            std::cerr << "[WARN] Buffer is full for consumer " << pair.first << " on topic " << topic_id << std::endl;
+            cerr << "[WARN] Buffer is full for consumer " << pair.first << " on topic " << topic_id << endl;
         }
     }
 }
 
 bool MessageBroker::consume(int64_t topic_id, int64_t consumer_id, Message& out) {
-    std::shared_ptr<TopicData> topic_data;
+    shared_ptr<TopicData> topic_data;
     {
-        std::lock_guard<std::mutex> lock(map_mtx);
+        lock_guard<mutex> lock(map_mtx);
         auto it = topics.find(topic_id);
         if (it == topics.end()) {
             return false;
@@ -52,7 +54,7 @@ bool MessageBroker::consume(int64_t topic_id, int64_t consumer_id, Message& out)
         topic_data = it->second;
     } 
 
-    std::lock_guard<std::mutex> lock(topic_data->topic_mtx);
+    lock_guard<mutex> lock(topic_data->topic_mtx);
     auto consumer_it = topic_data->subscribers.find(consumer_id);
     if (consumer_it != topic_data->subscribers.end()) {
         return consumer_it->second.pop(out);
@@ -60,9 +62,9 @@ bool MessageBroker::consume(int64_t topic_id, int64_t consumer_id, Message& out)
     return false;
 }
 
-bool MessageBroker::consume_batch(int64_t topic_id, int64_t consumer_id, std::vector<Message>& out, size_t max_items) {
+bool MessageBroker::consume_batch(int64_t topic_id, int64_t consumer_id, vector<Message>& out, size_t max_items) {
     out.clear();
-    std::shared_ptr<TopicData> topic_data;
+    shared_ptr<TopicData> topic_data;
     {
         std::lock_guard<std::mutex> lock(map_mtx);
         auto it = topics.find(topic_id);
@@ -74,7 +76,7 @@ bool MessageBroker::consume_batch(int64_t topic_id, int64_t consumer_id, std::ve
 
     out.reserve(max_items);
 
-    std::lock_guard<std::mutex> lock(topic_data->topic_mtx);
+    lock_guard<mutex> lock(topic_data->topic_mtx);
     auto consumer_it = topic_data->subscribers.find(consumer_id);
     if (consumer_it != topic_data->subscribers.end()) {
         Message msg;
--- MessageBroker.hpp
+++ MessageBroker.hpp
@@ -9,6 +9,8 @@
 #include <cstdint>
 #include <chrono>
 
+using namespace std;
+
 #include "RingBuffer.hpp"
 
 struct Message {
@@ -20,26 +22,26 @@ struct Message {
 };
 
 inline uint64_t getCurrentTimestamp() {
-    return std::chrono::duration_cast<std::chrono::nanoseconds>(
-               std::chrono::high_resolution_clock::now().time_since_epoch())
+    return chrono::duration_cast<chrono::nanoseconds>(
+               chrono::high_resolution_clock::now().time_since_epoch())
         .count();
 }
 
 struct TopicData {
-    std::mutex topic_mtx;
-    std::unordered_map<int64_t, RingBuffer<Message, 1024>> subscribers;
+    mutex topic_mtx;
+    unordered_map<int64_t, RingBuffer<Message, 1024>> subscribers;
 };
 
 class MessageBroker {
 private:
-    std::mutex map_mtx;
-    std::unordered_map<int64_t, std::shared_ptr<TopicData>> topics;
+    mutex map_mtx;
+    unordered_map<int64_t, shared_ptr<TopicData>> topics;
 
 public:
     void subscribe(int64_t topic_id, int64_t consumer_id);
-    void publish(int64_t type, int64_t topic_id, const std::string& data);
+    void publish(int64_t type, int64_t topic_id, const string& data);
     bool consume(int64_t topic_id, int64_t consumer_id, Message& out);
-    bool consume_batch(int64_t topic_id, int64_t consumer_id, std::vector<Message>& out, size_t max_items);
+    bool consume_batch(int64_t topic_id, int64_t consumer_id, vector<Message>& out, size_t max_items);
 };
 
 #endif // MESSAGE_BROKER_HPP
